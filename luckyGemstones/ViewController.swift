caa8d929d82fe0f4b763df0fea61f88d41ab91a3
Testing_author <testing_beta@mail.com>
2019-08-14 21:13:33 +0300

    Initial Commit

---
+++ luckyGemstones/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  luckyGemstones
+//
+//  Created by DDDD on 14/08/2019.
+//  Copyright © 2019 MeerkatWorks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
