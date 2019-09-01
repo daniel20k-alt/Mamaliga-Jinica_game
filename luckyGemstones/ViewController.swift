43b8b4cc7f2609250bbeb3f90feddefab45ce328
Testing_author <testing_beta@mail.com>
2019-09-01 12:33:50 +0300

    Finished the luckyGemstones project and also updated the player's and the CPU's displays when drawing cards from the shuffle. Each player gets 1 points for winning, and no points awarded for ties.

--- luckyGemstones/ViewController.swift
+++ luckyGemstones/ViewController.swift
@@ -18,6 +18,10 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var rightScoreLabel: UILabel!
     
+        var rightScore = 0
+        var leftScore = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -34,15 +38,24 @@ class ViewController: UIViewController {
         // print(rightNumber)
    
         leftImageView.image = UIImage(named: "card\(leftNumber)")
-        
+        //updating left image view
         rightImageView.image = UIImage(named: "card\(rightNumber)")
-    
+        //updating right image view
+        
+        if leftNumber > rightNumber {
+            //Left side would win in this case
+    leftScore += 1
+    leftScoreLabel.text = String(leftScore)
+            
+        } else if leftNumber < rightNumber {
+            //Right side would win in this case
+    rightScore += 1
+    rightScoreLabel.text = String(rightScore)
+            
+        } else {
+            // i'll leave it to do nothing, this way no points will be added or substracted when it will be equal
+        }
     }
-    
-// testing
-    
-
-
-    
+      
 }
 
